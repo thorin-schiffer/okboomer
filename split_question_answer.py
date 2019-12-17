f2b0722b50d0349450b6f192f6a4421340a37959
Sergey Cheparev <sergey@cheparev.com>
2019-12-17 14:34:36 +0100

    Qa json

--- split_question_answer.py
+++ split_question_answer.py
@@ -5,8 +5,10 @@ with open("comments.json", "r") as f:
     comments = json.load(f)
 
 
-def split_newline(question, answer):
+def split(question, answer):
     parts = question.split("\n")
+    if len(parts) == 1:
+        parts = question.split("  ")
     parts = [part for part in parts if part]
     if len(parts) == 2:
         return parts
@@ -29,20 +31,40 @@ def remove_quotes(question, answer):
 def remove_edit(question, answer):
     if "edit:" in answer.lower():
         to = answer.lower().index("edit:")
-        answer = answer[:to]
+        answer = answer[to + 5:]
+    return question, answer
+
+
+def remove_ok_boomer(question, answer):
+    if "boomer" in question.lower():
+        to = question.lower().index("boomer")
+        question = question[to + 6:]
+        question = question.strip()
+        if question[0] in [".", ",", ":", " "]:
+            question = question[1:]
+        question = question.strip()
+        question = question.capitalize()
     return question, answer
 
 
 steps = [
-    split_newline,
+    split,
     remove_quotes,
-    remove_edit
+    remove_edit,
+    remove_ok_boomer
 ]
 
+parsed_comments = []
 for comment in comments:
     if comment['score'] < MIN_SCORE:
         continue
     question, answer = comment['body'], ""
     for step in steps:
         question, answer = step(question, answer)
-    print(f"question: {question}\n answer: {answer}")
+
+    comment['question'] = question
+    comment['answer'] = answer
+    parsed_comments.append(comment)
+
+with open("qa.json", "w") as f:
+    json.dump(parsed_comments, f)
